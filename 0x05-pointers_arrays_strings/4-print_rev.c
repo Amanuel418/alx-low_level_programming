902e734063a9070022d7f7ce72a8630ab8cd842a
Amanuel <amanuelsolomon@gmail.com>
2022-06-13 16:49:36 -0700

    444444

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,17 +6,14 @@
 void print_rev(char *s)
 {
 	int i;
-	int x;
 
-	for (i = 0; s[i] != 0; i++)
-	;
-	x = i;
-	for (int j = x; s[j] == s[0]; j--)
-	{
-		if (s[j] == 0)
-			continue;
-
-		_putchar(s[j]);
-	}
-	_putchar('\n');
+for (i = 0; s[i] != 0; i++)
+;
+i = i - 1;
+while (i >= 0)
+{
+_putchar(s[i]);
+i--;
+}
+_putchar('\n');
 }
