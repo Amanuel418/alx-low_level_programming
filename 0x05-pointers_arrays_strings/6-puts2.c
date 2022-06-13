d5b87ef5124b36b9e53edee859d1f912bd9bc69c
Amanuel <amanuelsolomon@gmail.com>
2022-06-13 16:57:46 -0700

    3342

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - updates the value it points to to 98
+ * @str: character value.
+ */
+void puts2(char *str)
+{
+int i;
+i
+for (i = 0; str[i] != 0; i + 2)
+_putchar(str[i]);
+_putchar('\n');
+}
