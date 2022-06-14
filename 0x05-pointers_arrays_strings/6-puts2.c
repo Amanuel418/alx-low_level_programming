e6ae7a1139cc150b0ee9af0990aadc325253d68b
Amanuel <amanuelsolomon@gmail.com>
2022-06-13 19:14:07 -0700

    3423

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _puts - updates the value it points to to 98
+ * _puts2 - updates the value it points to to 98
  * @str: character value.
  */
 void puts2(char *str)
 {
 int i;
 
-for (i = 0; str[i] != 0; i += 2)
+for (i = 0; str[i] != '\0'; i += 2)
 _putchar(str[i]);
 _putchar('\n');
 }
