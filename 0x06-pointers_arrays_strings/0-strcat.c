835c94d52aefab9bdc2eadb1d2aa093b2338f11a
Amanuel <amanuelsolomon@gmail.com>
2022-06-15 15:07:21 -0700

    32

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,24 @@
 #include "main.h"
 /**
- * _strcat() - concatenates two strings.
- * @*dest: pointer.
- * @*src: pointer.
- * Return - string pointer.
+ * _strcat - concatenates two strings
+ * @dest: input parameter string
+ * @src: input parameter string
+ *
+ * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
 	int i;
 	int x;
 	int j;
-	char *aman;
 
 	for (i = 0; dest[i] != 0; i++)
 	;
 	for (j = 0; src[j] != 0; j++)
 	{
-        	dest[i]=src[j];
-	      	i++;
+		dest[i] = src[j];
+		i++;
 	}
-	aman = dest[i];
-	return (aman);
+	dest[i] = '\0';
+	return (dest);
 }
