6ba7cc4aa16e8c70813c0c64c5073d5276b5c920
Amanuel <amanuelsolomon@gmail.com>
2022-06-15 16:02:06 -0700

    32234

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,12 +14,10 @@ int j;
 for (i = 0; dest[i] != 0; i++)
 ;
 
-for (j = 0; src[j] != 0; j++)
+for (j = 0; src[j] != 0 && j < n; j++)
 {
 dest[i] = src[j];
 i++;
-if (j == n - 1)
-break;
 }
 return (dest);
 }
