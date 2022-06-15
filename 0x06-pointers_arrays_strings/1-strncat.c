1fcc1be81c410cfdafb0316ef65e5a1872b7a650
Amanuel <amanuelsolomon@gmail.com>
2022-06-15 15:55:02 -0700

    9s

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,18 +8,18 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
+int i;
+int j;
 
-	for (i = 0; dest[i] != 0; i++)
-	;
-	for (j = 0; src[j] != 0; j++)
-	{
-		dest[i] = src[j];
-		i++;
-		if (j == n)
-			break;
-	}
-	dest[i] = '\0';
-	return (dest);
+for (i = 0; dest[i] != 0; i++)
+;
+
+for (j = 0; src[j] != 0; j++)
+{
+dest[i] = src[j];
+i++;
+if (j == n)
+break;
+}
+return (dest);
 }
