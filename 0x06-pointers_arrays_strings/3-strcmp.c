85c788665b4802f0d2457d4d00d48e1a1bf46e1b
Amanuel <amanuelsolomon@gmail.com>
2022-06-15 22:47:43 -0700

    32s

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,7 +9,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != 0; i++)
+	for (i = 0; s1[i] != 0 && s2[i] != 0; i++)
 	{
 		if (s1[i] > s2[i])
 		{
@@ -23,6 +23,14 @@ int _strcmp(char *s1, char *s2)
 		{
 			continue;
 		}
+	}
+	if (s1[i] == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (-1);
 	}
 	return (0);
 }
