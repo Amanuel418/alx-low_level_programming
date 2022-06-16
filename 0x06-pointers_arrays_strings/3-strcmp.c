93248c4a7b392e92ec37b5e29e5b0b7707254b5d
Amanuel <amanuelsolomon@gmail.com>
2022-06-15 22:15:46 -0700

    3234

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strcmp - compares two strings.
+ * _strcmp - compares two strings
  * @s1: string 1.
  * @s2: string 2.
  * Return : -1 , 0 , 1.
@@ -24,8 +24,5 @@ int _strcmp(char *s1, char *s2)
 			continue;
 		}
 	}
-	if (s1[i] == s2[i])
-	{
-		return (0);
-	}
+	return (0);
 }
