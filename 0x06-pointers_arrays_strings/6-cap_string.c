c48d4986c78b9fc3c4a5193b0c953090d1b866f5
Amanuel <amanuelsolomon@gmail.com>
2022-06-16 19:33:04 -0700

    8a9dh

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,24 +1,29 @@
 #include "main.h"
 /**
  * cap_string - capitalizes all words of a string
- * @n: the string to change the first letter of a word in uppercase
+ * @str: the string to change the first letter of a word in uppercase
  *
  * Return: capitalizes letters
  */
-char *cap_string(char *n)
+char *cap_string(char *str)
 {
-	int i;
+int i;
+int j;
+char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
 
-	for (i = 0; n[i] != 0; i++)
+for (i = 0; str[i] != '\0'; i++)
+{
+if (i == 0 && str[i] >= 97 && str[i] <= 122)
+{
+str[i] = str[i] - 32;
+}
+for (j = 0; c[j] != '\0'; j++)
 	{
-		if (i == 0 && n[i] >= 97 && n[i] <= 122)
-		{
-			n[i] -= 32;
-		}
-		else if (n[i - 1] == ' ' && n[i] >= 97 && n[i] <= 122)
-		{
-			n[i] -= 32;
-		}
-	}
-	return (n);
+if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
+{
+str[i + 1] = str[i + 1] - 32;
+}
+}
+}
+return (str);
 }
