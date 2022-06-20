92a13a81b69406bedf865c1d14816c4a4325c880
Amanuel <amanuelsolomon@gmail.com>
2022-06-20 15:49:35 -0700

    code for 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: pointer to a string
+ * @accept: pointer to a string
+ *
+ * Return: the number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	int sum;
+
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (accept[i] == s[j])
+			{
+				sum += 1;
+			}
+		}
+	}
+	return (sum);
+}
