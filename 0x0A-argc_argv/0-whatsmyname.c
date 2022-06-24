5c12082ad2ec03526a47ec1c4b89e131430a8896
Amanuel <amanuelsolomon@gmail.com>
2022-06-24 12:02:57 -0700

    3242

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 /**
  * main - Prints the name of the program
@@ -6,7 +5,7 @@
  * @argv: Array name
  * Return: 0
  */
-int main(int argc, **argv)
+int main(int argc, char **argv)
 {
 	printf("%s\n", argv[0]);
 	return (0);
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - is main.
+ * @argc: number of arguments passed
+ * @argv: array of strings of arguments passed
+ * return: integer
+ */
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
