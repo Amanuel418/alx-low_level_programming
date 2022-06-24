664d5e75b9471c5efa7d9aeb05af34650d07704d
Amanuel <amanuelsolomon@gmail.com>
2022-06-24 12:10:36 -0700

    322we

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,8 @@
  * @argv: Array name
  * Return: 0
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", argv[argc * 0]);
 	return (0);
 }
