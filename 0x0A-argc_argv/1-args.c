5ed7bd8fe6e52ec1eeb2c57ba22643c4451cdebc
Amanuel <amanuelsolomon@gmail.com>
2022-06-24 12:20:02 -0700

    12

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - is main.
  * @argc: number of arguments passed
  * @argv: array of strings of arguments passed
- * return: integer
+ * Return: 0
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	printf("%d\n", argc);
 	return (0);
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
-int main(int argc, **argv)
+#include "main.h"
+/**
+ * main - prints all arguments it recieves
+ * @argc: number of arguments
+ * @argv: array of string of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
 {
+	int i;
 
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i])
+	}
+	return (0);
+}
